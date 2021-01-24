97cbabd9c2e42cd2db84774df82138e95b132875
Julieta Colombo <julietanataliacolombo@yahoo.com.ar>
2021-01-24 18:36:07 -0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
+import pathlib
+
 from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
     name='scurri_zipcode',
     packages=find_packages(include=['scurri_zipcode']),
     version='0.1.0',
+    long_description=README,
+    long_description_content_type="text/markdown",
     description='Python library to validate zip codes in the UK for Scurri interview',
+    url="https://github.com/JuliColombo/scurri-zipcode",
     author='Julieta Colombo',
     license='MIT',
     install_requires=[],
